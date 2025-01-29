0716f427a2b6bbdbddfb964df35ea77e8ff6c773
jonhumphreys <93622145+jonhumphreys@users.noreply.github.com>
2025-01-28 18:34:46 -0800

    Initial Commit

--- Cpsc370Final/Program.cs
+++ Cpsc370Final/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length < 1)
+            Console.WriteLine("Usage: Cpsc370Final.exe <arguments>");
+        
+        // you can delete this if/when you like
+        ShowArguments(args);
+    }
+
+    // this is just an example of how to get the command
+    // line arguments so you can use them
+    private static void ShowArguments(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine("  Argument " + i +": " + args[i]);
+        }
     }
 }
